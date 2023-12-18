6a82ef7e8941632de65815953841ccfd3bdd7cb6
emilianogastaldo <emiliano0303@gmail.com>
2023-12-18 15:00:52 +0100

    Chiedo i dati all'utente

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log('JS OK');
+
+// Chiedo quantità km e anni dell'utente
+const kmTrip = parseInt(prompt('Quanti km devi percorrere?', 100).trim());
+console.log('km viaggio:', kmTrip);
+
+const userAge = prompt('Quanti anni hai?', 66).trim();
+console.log('età:', userAge);
+
