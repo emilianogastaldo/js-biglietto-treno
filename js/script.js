da9d61e766c2664f1b29f1b276d8b127867e9a8e
emilianogastaldo <emiliano0303@gmail.com>
2023-12-18 15:01:26 +0100

    calcolo il prezzo del biglietto

--- js/script.js
+++ js/script.js
@@ -7,3 +7,7 @@ console.log('km viaggio:', kmTrip);
 const userAge = prompt('Quanti anni hai?', 66).trim();
 console.log('età:', userAge);
 
+// Calcolo il prezzo del biglietto
+let priceTicket = kmTrip * 0.21;
+console.log('prezzo biglietto:', priceTicket);
+
